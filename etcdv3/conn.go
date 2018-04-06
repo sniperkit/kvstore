a6bc182e93e4b91a5ee5648499619827c50441ab
Michael Persson <michael.ake.persson@gmail.com>
2018-04-06 11:25:24 -0500

    should be good

--- etcdv3/conn.go
+++ etcdv3/conn.go
@@ -35,7 +35,7 @@ func (c *conn) Lease(ttl int) (kvstore.Lease, error) {
 	}, nil
 }
 
-func (c *conn) Set(key string, value interface{}) error {
+func (c *conn) Set(key string, value interface{}, options ...func(kvstore.KeyValue)) error {
 	b, err := json.Marshal(value)
 	if err != nil {
 		return fmt.Errorf("marshal value [%+v] for key [%s]: %v", value, key, err)
@@ -43,20 +43,6 @@ func (c *conn) Set(key string, value interface{}) error {
 
 	kvc := clientv3.NewKV(c.client)
 	if _, err := kvc.Put(context.TODO(), key, string(b)); err != nil {
-		return fmt.Errorf("set key [%s]: %v", key, err)
-	}
-	return nil
-}
-
-// TODO: pass lease as option to Set(key, value, opts...).
-func (c *conn) SetWithLease(key string, value interface{}, l kvstore.Lease) error {
-	b, err := json.Marshal(value)
-	if err != nil {
-		return fmt.Errorf("marshal value [%+v] for key [%s]: %v", value, key, err)
-	}
-
-	kvc := clientv3.NewKV(c.client)
-	if _, err := kvc.Put(context.TODO(), key, string(b), clientv3.WithLease(l.(*lease).id)); err != nil {
 		return fmt.Errorf("set key [%s]: %v", key, err)
 	}
 	return nil
@@ -93,7 +79,7 @@ func (c *conn) Values(key string) (kvstore.KeyValues, error) {
 	values := kvstore.KeyValues{}
 	for _, kv := range resp.Kvs {
 		// TODO: add TTL for lease, if leaseID is 0 set nil for no lease.
-		values = append(values, &keyValue{Key: string(kv.Key), Lease: &lease{id: clientv3.LeaseID(kv.Lease)}, Value: kvstore.Value(kv.Value)})
+		values = append(values, &keyValue{key: string(kv.Key), lease: &lease{id: clientv3.LeaseID(kv.Lease)}, value: kvstore.Value(kv.Value)})
 	}
 
 	return values, nil
--- etcdv3/keyvalue.go
+++ etcdv3/keyvalue.go
@@ -5,18 +5,35 @@ import (
 )
 
 type keyValue struct {
-	Key   string
-	Lease kvstore.Lease
-	TTL   int
-	Prev  kvstore.Value
+	key   string
+	lease kvstore.Lease
+	ttl   int
+	prev  kvstore.Value
+	value kvstore.Value
 	*kvstore.Event
-	kvstore.Value
 }
 
 type keyValues []*keyValue
 
+func (kv keyValue) Key() string {
+	return kv.key
+}
+
+func (kv keyValue) Value() kvstore.Value {
+	return kv.value
+}
+
+func (kv keyValue) Lease() kvstore.Lease {
+	return kv.lease
+}
+
+func (kv keyValue) TTL() int {
+	// TODO: if no lease return 0
+	return kv.lease.TTL()
+}
+
 func (kv keyValue) SetLease(lease kvstore.Lease) error {
-	kv.Lease = lease
+	kv.lease = lease
 	return nil
 }
 
