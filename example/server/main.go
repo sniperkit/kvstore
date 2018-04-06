dcf318d2d1412f394bf45f4f128aec7a3c9669bf
Michael Persson <michael.ake.persson@gmail.com>
2018-04-06 15:54:27 -0500

    partial server example

---
+++ example/server/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/docopt/docopt-go"
+	"github.com/mickep76/kvstore"
+	_ "github.com/mickep76/kvstore/etcdv3"
+)
+
+type Host struct {
+	Hostname string `json:"hostname"`
+}
+
+func main() {
+	usage := `client
+
+Usage:
+  client [--backend=<backend>] [--prefix=<prefix>] [--endpoints=<endpoints>] [--timeout=<seconds>] [--keepalive=<seconds>]
+  client -h | --help
+  client --version
+
+Options:
+  -h --help                             Show this screen.
+  --version                             Show version.
+  --backend=<backend>                   Key/value store backend. [default: etcdv3]
+  --prefix=<prefix>                     Key/value store prefix. [default: /dock2box]
+  --endpoints=<endpoints>               Comma-delimited list of hosts in the key/value store cluster. [default: 127.0.0.1:2379]
+  --timeout=<seconds>                   Connection timeout for key/value cluster in seconds. [default: 5]
+  --keepalive=<seconds>                 Connection keepalive for key/value cluster in seconds. [default: 60]
+`
+
+	// Parse arguments.
+	args, err := docopt.Parse(usage, nil, true, "client 0.0.1", false)
+	if err != nil {
+		log.Fatalf("parse args: %v", err)
+	}
+
+	// Get timeout.
+	timeout, err := strconv.Atoi(args["--timeout"].(string))
+	if err != nil {
+		log.Fatalf("strconv: %v", err)
+	}
+
+	// Get keepalive.
+	keepalive, err := strconv.Atoi(args["--keepalive"].(string))
+	if err != nil {
+		log.Fatalf("strconv: %v", err)
+	}
+
+	// Get prefix.
+	prefix := args["--prefix"].(string)
+
+	// Connect to etcd.
+	kvc, err := kvstore.Open("etcdv3", strings.Split(args["--endpoints"].(string), ","), kvstore.Timeout(timeout))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create watch for new hosts.
+}
