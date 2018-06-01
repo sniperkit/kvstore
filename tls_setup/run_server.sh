629bb50b91ca32c49c68513a9146836133c5a4de
Michael Persson <michael.ake.persson@gmail.com>
2018-06-01 12:13:09 -0500

    fix test exec

--- tls_setup/run_server.sh
+++ tls_setup/run_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-[ ! -e ../example/server/server ] && (cd ../example/server && go build)
+[ ! -x ../example/server/server ] && (cd ../example/server && go build)
 
 ../example/server/server -ca certs/ca.pem -cert certs/example_server.pem -key certs/example_server.key
