4f99a81ea185685022f9431c81a41166af8348b4
Michael Persson <michael.ake.persson@gmail.com>
2018-06-01 11:18:59 -0500

    rename

--- tls_setup/test_curl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --cacert certs/ca.pem --cert certs/etcd.pem --key certs/etcd.key https://127.0.0.1:2379/v2/keys/foo -XPUT -d value=bar -v
--- tls_setup/test_etcdctl.sh
+++ tls_setup/test_etcdctl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ETCDCTL="etcdctl --cacert certs/ca.pem --cert certs/d2b_server.pem --key certs/d2b_server.key --endpoints https://localhost:2379"
+ETCDCTL="etcdctl --cacert certs/ca.pem --cert certs/example_server.pem --key certs/example_server.key --endpoints https://localhost:2379"
 
 export ETCDCTL_API=3
 
@@ -9,7 +9,7 @@ ${ETCDCTL} put /dock2box/test "my value"
 echo "get"
 ${ETCDCTL} get --prefix /dock2box
 
-ETCDCTL="etcdctl --cacert certs/ca.pem --cert certs/d2b_client.pem --key certs/d2b_client.key --endpoints https://localhost:2379"
+ETCDCTL="etcdctl --cacert certs/ca.pem --cert certs/example_client.pem --key certs/example_client.key --endpoints https://localhost:2379"
 
 echo "put"
 ${ETCDCTL} put /dock2box/test "my value"
