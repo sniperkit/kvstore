64e8f57ce05303c34aa34820601ce60907396a13
Michael Persson <michael.ake.persson@gmail.com>
2018-06-14 09:38:50 -0500

    rename ConnOption to DriverOption add KeyValueOption type

--- conn.go
+++ conn.go
@@ -9,7 +9,7 @@ import (
 type Conn interface {
 	Close() error
 	Lease(ttl int) (Lease, error)
-	Set(key string, value interface{}, options ...func(KeyValue) error) error
+	Set(key string, value interface{}, options ...KeyValueOption) error
 	Delete(key string) error
 	Keys(path string) ([]string, error)
 	//	Value(path string) (KeyValue, error)
@@ -17,11 +17,11 @@ type Conn interface {
 	Watch(path string) Watch
 }
 
-// ConnOption option for constructor.
-type ConnOption func(Driver) error
+// DriverOption option for constructor.
+type DriverOption func(Driver) error
 
 // Open connection to database.
-func Open(driver string, endpoints []string, options ...ConnOption) (Conn, error) {
+func Open(driver string, endpoints []string, options ...DriverOption) (Conn, error) {
 	d, ok := drivers[driver]
 	if !ok {
 		return nil, fmt.Errorf("driver is not registered: %s", driver)
@@ -37,56 +37,58 @@ func Open(driver string, endpoints []string, options ...ConnOption) (Conn, error
 }
 
 // WithTimeout for database connection.
-func WithTimeout(timeout int) ConnOption {
+func WithTimeout(timeout int) DriverOption {
 	return func(d Driver) error {
 		return d.SetTimeout(timeout)
 	}
 }
 
 // WithTLS config for database connection.
-func WithTLS(config *tls.Config) ConnOption {
+func WithTLS(config *tls.Config) DriverOption {
 	return func(d Driver) error {
 		return d.SetTLS(config)
 	}
 }
 
 // WithUser for database connection.
-func WithUser(user string) ConnOption {
+func WithUser(user string) DriverOption {
 	return func(d Driver) error {
 		return d.SetUser(user)
 	}
 }
 
 // WithPassword for database connection.
-func WithPassword(password string) ConnOption {
+func WithPassword(password string) DriverOption {
 	return func(d Driver) error {
 		return d.SetPassword(password)
 	}
 }
 
 // WithEncoding to encode values.
-func WithEncoding(encoding string) ConnOption {
+func WithEncoding(encoding string) DriverOption {
 	return func(d Driver) error {
 		return d.SetEncoding(encoding)
 	}
 }
 
 // WithPrefix prefix pre+pended to key path.
-func WithPrefix(prefix string) ConnOption {
+func WithPrefix(prefix string) DriverOption {
 	return func(d Driver) error {
 		return d.SetPrefix(prefix)
 	}
 }
 
+type KeyValueOption func(KeyValue) error
+
 // WithLease for key/value.
-func WithLease(lease Lease) func(KeyValue) error {
+func WithLease(lease Lease) KeyValueOption {
 	return func(kv KeyValue) error {
 		return kv.SetLease(lease)
 	}
 }
 
 // WithTTL for key/value.
-func WithTTL(ttl int) func(KeyValue) error {
+func WithTTL(ttl int) KeyValueOption {
 	return func(kv KeyValue) error {
 		return kv.SetTTL(ttl)
 	}
