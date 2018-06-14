f0b2bb0209f6a746a976c3ee789b54ee0d13dcbd
Michael Persson <michael.ake.persson@gmail.com>
2018-06-14 09:32:35 -0500

    add ConnOptions type

--- conn.go
+++ conn.go
@@ -17,8 +17,11 @@ type Conn interface {
 	Watch(path string) Watch
 }
 
+// ConnOption option for constructor.
+type ConnOption func(Driver) error
+
 // Open connection to database.
-func Open(driver string, endpoints []string, options ...func(Driver) error) (Conn, error) {
+func Open(driver string, endpoints []string, options ...ConnOption) (Conn, error) {
 	d, ok := drivers[driver]
 	if !ok {
 		return nil, fmt.Errorf("driver is not registered: %s", driver)
@@ -34,42 +37,42 @@ func Open(driver string, endpoints []string, options ...func(Driver) error) (Con
 }
 
 // WithTimeout for database connection.
-func WithTimeout(timeout int) func(Driver) error {
+func WithTimeout(timeout int) ConnOption {
 	return func(d Driver) error {
 		return d.SetTimeout(timeout)
 	}
 }
 
 // WithTLS config for database connection.
-func WithTLS(config *tls.Config) func(Driver) error {
+func WithTLS(config *tls.Config) ConnOption {
 	return func(d Driver) error {
 		return d.SetTLS(config)
 	}
 }
 
 // WithUser for database connection.
-func WithUser(user string) func(Driver) error {
+func WithUser(user string) ConnOption {
 	return func(d Driver) error {
 		return d.SetUser(user)
 	}
 }
 
 // WithPassword for database connection.
-func WithPassword(password string) func(Driver) error {
+func WithPassword(password string) ConnOption {
 	return func(d Driver) error {
 		return d.SetPassword(password)
 	}
 }
 
 // WithEncoding to encode values.
-func WithEncoding(encoding string) func(Driver) error {
+func WithEncoding(encoding string) ConnOption {
 	return func(d Driver) error {
 		return d.SetEncoding(encoding)
 	}
 }
 
 // WithPrefix prefix pre+pended to key path.
-func WithPrefix(prefix string) func(Driver) error {
+func WithPrefix(prefix string) ConnOption {
 	return func(d Driver) error {
 		return d.SetPrefix(prefix)
 	}
